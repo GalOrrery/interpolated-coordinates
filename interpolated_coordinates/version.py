48046c04a9cff3c3c0364e3524f72f41475f145b
nstarman <nstarkman@protonmail.com>
2022-02-03 23:44:51 -0500

    style fixes
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- interpolated_coordinates/version.py
+++ interpolated_coordinates/version.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 # Note that we need to fall back to the hard-coded version if either
 # setuptools_scm can't be imported or setuptools_scm can't determine the
 # version, so we catch the generic 'Exception'.
 try:
+    # THIRD PARTY
     from setuptools_scm import get_version
-    version = get_version(root='..', relative_to=__file__)
+
+    version = get_version(root="..", relative_to=__file__)
 except Exception:
-    version = '0.1.dev38+g97d6628'
+    version = "0.1.dev38+g97d6628"
